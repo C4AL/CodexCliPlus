5b7c3a9e6031fe6f267638471f889a32bdbf2e35
C4AL <104809382+C4AL@users.noreply.github.com>
2026-04-28 18:51:00 +0800

    Update CLIProxyAPI backend to 6.9.40

--- src/CodexCliPlus.Core/Constants/BackendReleaseMetadata.cs
+++ src/CodexCliPlus.Core/Constants/BackendReleaseMetadata.cs
@@ -2,18 +2,18 @@ namespace CodexCliPlus.Core.Constants;
 
 public static class BackendReleaseMetadata
 {
-    public const string Version = "6.9.36";
+    public const string Version = "6.9.40";
 
-    public const string ReleaseTag = "v6.9.36";
+    public const string ReleaseTag = "v6.9.40";
 
-    public const string AssetName = "CLIProxyAPI_6.9.36_windows_amd64.zip";
+    public const string AssetName = "CLIProxyAPI_6.9.40_windows_amd64.zip";
 
     public const string ReleaseUrl =
-        "https://github.com/router-for-me/CLIProxyAPI/releases/tag/v6.9.36";
+        "https://github.com/router-for-me/CLIProxyAPI/releases/tag/v6.9.40";
 
     public const string ArchiveUrl =
-        "https://github.com/router-for-me/CLIProxyAPI/releases/download/v6.9.36/CLIProxyAPI_6.9.36_windows_amd64.zip";
+        "https://github.com/router-for-me/CLIProxyAPI/releases/download/v6.9.40/CLIProxyAPI_6.9.40_windows_amd64.zip";
 
     public const string ArchiveSha256 =
-        "97f3ccc20e2b6fe35faa02732ca6497ccf13c57eac08ed94263682545515a2c4";
+        "b21eb12a819f49fc43affc29a3996810be85c090ba0c523b7704ce90070ae760";
 }
